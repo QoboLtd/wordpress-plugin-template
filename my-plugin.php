e606f89c80cde36882529512857a9dc9d2c18269
Leonid Mamchenkov <leonid@mamchenkov.net>
2016-11-20 19:16:31 +0200

    Added plugin source code placeholders

---
+++ my-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: My Plugin
+ * Plugin URI:
+ * Description: My Plugin
+ * Version: 1.0.0
+ *
+ * @package My_Plugin
+ */
+
+// Require plugin code.
+require 'src' . DIRECTORY_SEPARATOR . 'plugin.php';
+
---
+++ src/plugin.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * PHP5
+ *
+ * The main plugin file.
+ *
+ * @package My_Plugin
+ */
+
+/**
+ * Require plugin files.
+ */
+// require_once __DIR__ . DIRECTORY_SEPARATOR . 'my-plugin.php'; // Placeholder.
+/**
+ * Run plugin code
+ *
+ * This function is called from the plugin file
+ * in the parent directory.
+ *
+ * @return void
+ */
+function qb_plugin_run() {
+	// Execute plugin code.
+}
+
+// Run plugin code.
+qb_plugin_run();
